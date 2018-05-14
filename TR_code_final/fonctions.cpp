480d5f3b3589ab40e5c43042a61c9f5ef9b7528f
leolau64@gmail.com <leolau64@gmail.com>
2018-05-14 18:18:00 +0200

    Edit

--- TR_code_final/fonctions.cpp
+++ TR_code_final/fonctions.cpp
@@ -441,7 +441,7 @@ void Affichage(void *arg){
 
 void Communication_Android (void *arg){
     
-    rt_printf("Thread ANDROID : Debut de l'éxecution\n");
+   /* rt_printf("Thread ANDROID : Debut de l'éxecution\n");
     log_task_entered();
     
     float puissance = 0.0;
@@ -469,6 +469,132 @@ void Communication_Android (void *arg){
         
         
     }
+    */
+    
+    void Communication_Android (void *arg){
+    
+    rt_printf("Thread ANDROID : Debut de l'éxecution de periodique à 100 Hz\n");
+    rt_task_set_periodic(NULL, TM_NOW, 10000000);
+
+    log_task_entered();
+    
+    float puissance = 0.0;
+    float angle = 0.0;
+    int socket_desc , client_sock , c , read_size, sens;
+    struct sockaddr_in server , client;
+    char client_message[2000];
+
+    //Create socket
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    if (socket_desc == -1)
+    {
+        printf("Could not create socket");
+    }
+    puts("Socket created");
+
+    //Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons( 8888 );
+
+    //Bind
+    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        //print the error message
+        perror("bind failed. Error");
+        //return 1;
+    }
+    puts("bind done");
+	while (1){
+    //Listen
+    listen(socket_desc , 3);
+
+    //Accept and incoming connection
+    puts("Waiting for incoming connections...");
+    c = sizeof(struct sockaddr_in);
+
+    //accept connection from an incoming client
+    client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
+    if (client_sock < 0)
+    {
+        perror("accept failed");
+        //return 1;
+    }
+    puts("Connection accepted");
+	       
+    int MAX_SIZE = 1024;
+    int len,i,taille;
+    char tab[MAX_SIZE];
+    char subtab[3];
+    char string[MAX_SIZE];
+    float data;
+
+	/* lecture d'un message de taille max MAX_SIZE, information dans string */
+ 	//len = read(sckt, string, MAX_SIZE);
+	while( (len = recv(client_sock , string , MAX_SIZE , 0)) > 0 ){
+		taille=strlen((char*)string);
+		if(taille > 0){
+			memcpy(tab,string,taille);	//Si le message n'est pas vide, on copie ces informations dans tab
+
+			for(i=0 ; i<len ; i++){
+					if((tab[i] == '<')&&(tab[i+6] == '\n')){
+						switch (tab[i+1]){
+							//puissance
+							case 'p':
+									memcpy(&data,&tab[i+2],sizeof(data));
+									subtab[0] = tab[i+2];
+									subtab[1] = tab[i+3];
+									subtab[2] = tab[i+4];
+									subtab[3] = tab[i+5];
+									puissance = atof(subtab); 
+									break;
+							//angle		
+							case 'a':
+									memcpy(&data,&tab[i+2],sizeof(data));
+									subtab[0] = tab[i+2];
+									subtab[1] = tab[i+3];
+									subtab[2] = tab[i+4];
+									subtab[3] = tab[i+5];
+									angle = atof(subtab); 
+									break;
+							//sens
+							case 's':
+									//puts("S \n");
+									memcpy(&data,&tab[i+2],sizeof(data));
+									subtab[0] = tab[i+2];
+									subtab[1] = tab[i+3];
+									subtab[2] = tab[i+4];
+									subtab[3] = tab[i+5];
+									sens = atoi(subtab);
+									break;
+
+						}//case
+					}//<\n
+			} //for
+		} //if taille
+		printf("Puissance : %f \nAngle : %f \nSens : %d \n", puissance, angle, sens);
+                puissance = k1*30*puissance;
+                rt_mutex_acquire(&var_mutex_consigne_couple, TM_INFINITE);
+                log_mutex_acquired(&var_mutex_consigne_couple);
+
+                consigne_couple.set_consigne(puissance);
+
+                rt_mutex_release(&var_mutex_consigne_couple);
+                log_mutex_released(&var_mutex_consigne_couple);
+	} //while 
+        
+        if(read_size == 0)
+        {
+            puts("Client disconnected");
+            fflush(stdout);
+        }
+        else if(read_size == -1)
+        {
+            perror("recv failed");
+        }
+    }
+
+}
     
         
      
