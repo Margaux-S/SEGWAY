1fb7aafaea2b82a6404261117ef526b1d9ac54bc
leolau64@gmail.com <leolau64@gmail.com>
2018-05-15 16:22:04 +0200

    Edit

--- TR_code_final/fonctions.cpp
+++ TR_code_final/fonctions.cpp
@@ -325,10 +325,9 @@ rt_printf("6");
 		rt_task_wait_period(NULL);
 
 		message_stm m;
-                //rt_printf("Prequeue read\n");
 		int err = rt_queue_read(&queue_Msg2STM,&m,sizeof(message_stm),SECENTOP / 10000);
-                //
-                rt_printf("Postqueue read\n");
+                
+                
 
 		if(m.label == 'c'){
 			send_float_to_serial(m.fval,'c');
