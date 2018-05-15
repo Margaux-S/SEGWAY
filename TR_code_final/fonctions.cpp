3de35d0e0be5000c2cc145845ba375918dcfcaea
leolau64@gmail.com <leolau64@gmail.com>
2018-05-15 17:39:06 +0200

    Edit

--- TR_code_final/fonctions.cpp
+++ TR_code_final/fonctions.cpp
@@ -546,7 +546,7 @@ void Communication_Android (void *arg){
     rt_mutex_acquire(&var_mutex_consigne_couple, TM_INFINITE);
     log_mutex_acquired(&var_mutex_consigne_couple);
 
-    consigne_couple.set_consigne(0.0);
+    consigne_couple.set_consigne(0.001);
     
     rt_mutex_release(&var_mutex_consigne_couple);
     log_mutex_released(&var_mutex_consigne_couple);
@@ -608,7 +608,7 @@ void Communication_Android (void *arg){
                 rt_mutex_acquire(&var_mutex_consigne_couple, TM_INFINITE);
                 log_mutex_acquired(&var_mutex_consigne_couple);
 
-                consigne_couple.set_consigne(puissance*10*0.80435);
+                consigne_couple.set_consigne(puissance*3*0.80435);
 
                 rt_mutex_release(&var_mutex_consigne_couple);
                 log_mutex_released(&var_mutex_consigne_couple);
